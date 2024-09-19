c9b5c8517d23f9de0804b5eb1db25da4be8b8dbc
nt_56 <nagutirth2121@gmail.com>
2024-09-19 20:46:12 +0530

    UI Completed

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,43 @@
-import React from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
+import { useState } from "react";
 
 const ProfileUpdate = () => {
-  return <div>ProfileUpdate</div>;
+  const [image, setImage] = useState(false);
+
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg, .PNG, .JPG, .JPEG"
+              hidden
+            />
+            <img
+              src={
+                image ? window.URL.createObjectURL(image) : assets.avatar_icon
+              }
+              alt=""
+            />
+            upload profile image
+          </label>
+          <input type="text" placeholder="your name" required />
+          <textarea placeholder="your bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
